51dcbcfbd04b0d2e4e57294ed97d418a5f03216a
Andrei <andreikagit@users.noreply.github.com>
2021-10-09 17:23:01 +0300

    copy & create scripts, initial version

--- step2_copy_proteus125honda.sh
+++ step2_copy_proteus125honda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "proteus-Honda-K-125-adapter" "a"
+python2.7 ./bin/copy_from_Kicad.py "frames:proteus" ".." "../../gerber" "125honda" "a"
 
 echo "Done!"
--- step3_create_proteus125honda.sh
+++ step3_create_proteus125honda.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board.sh ".." "88bmw" "a" "bom_replace_88bmw-a.csv" "80,0"
+sh bin/create_board_with_prefix.sh "proteus" "../proteus125honda" "125honda" "a" "bom_replace_125honda-a.csv" "80,0"
